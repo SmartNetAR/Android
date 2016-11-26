0b342a63cb42587bd1120a8d5c01fc165e50a122
SmartNetAR <leonardo@smartnet.com.ar>
2016-11-26 00:28:55 -0300

    agrego clase ListaArticulos

--- TP_Final_V2/app/src/main/java/ar/com/smartnet/tp_final_v2/EditaPedidoActivity.java
+++ TP_Final_V2/app/src/main/java/ar/com/smartnet/tp_final_v2/EditaPedidoActivity.java
@@ -1,10 +1,20 @@
 package ar.com.smartnet.tp_final_v2;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class EditaPedidoActivity extends AppCompatActivity {
 
@@ -12,7 +22,10 @@ public class EditaPedidoActivity extends AppCompatActivity {
     private EditText edit_codigo ;
     private EditText edit_cantidad ;
     private EditText edit_importe ;
+    private Button btn_add ;
     private ListView lista_pedido ;
+    private ArrayList<Articulo> articulos ;
+    private ArticulosAdapter adapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,79 @@ public class EditaPedidoActivity extends AppCompatActivity {
         edit_cantidad = (EditText) findViewById(R.id.editCantidad) ;
         edit_importe = (EditText) findViewById(R.id.editImporte) ;
         lista_pedido = (ListView) findViewById(R.id.lista_pedidos) ;
+        lista_pedido.setAdapter( adapter );
+        btn_add = (Button) findViewById(R.id.btnAdd) ;
+        articulos = new ArrayList<>() ;
 
     }
+
+
+    public void OnAddArticulo (View view) {
+        articulos.add(new Articulo(
+                edit_codigo.toString(),
+                "descripcion",
+                Float.valueOf(edit_cantidad.toString()),
+                Float.valueOf(edit_importe.toString())
+        ));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edita_pedido_options, menu);
+        return true;
+    }
+
+
+    private class ArticulosAdapter extends ArrayAdapter<Articulo> {
+        ArticulosAdapter() {
+            super(EditaPedidoActivity.this, R.layout.item_lista_edita_pedido, articulos);
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View result = convertView ;
+            if (result == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                result = inflater.inflate(R.layout.item_lista_edita_pedido, parent, false);
+            }
+            Articulo articulo = getItem(position);
+
+            TextView titulo = (TextView) result.findViewById(R.id.titulo) ;
+            /*titulo.setText(
+                    pedido.getCliente().toString() + "  $ " +
+                            (String) String.valueOf(pedido.getImporteTotal())
+            ) ;
+            */
+            titulo.setText( articulo.getDescripcion().toString() );
+            TextView resumen = (TextView) result.findViewById(R.id.resumen);
+            resumen.setText( String.valueOf( articulo.getCantidad()) ) ;
+            //(String) String.valueOf(pedido.getImporteTotal())) ;//.replace("\n", " "));
+            return result ;
+        }
+    }
+
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.guardar:
+                String titulo =  edit_titulo.getText().toString();
+                String texto =  edit_texto.getText().toString();
+                if (pos != -1) {
+                    ListaNotas.modifica(pos, titulo, texto);
+                }else {
+                    ListaNotas.nueva(titulo, texto);
+                }
+                setResult(RESULT_OK);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    */
 }
---
+++ TP_Final_V2/app/src/main/java/ar/com/smartnet/tp_final_v2/ListaArticulos.java
@@ -0,0 +1,50 @@
+package ar.com.smartnet.tp_final_v2;
+
+import java.util.ArrayList;
+
+/**
+ * Created by leo on 26/11/2016.
+ */
+
+public class ListaArticulos {
+    private static ArrayList<Articulo> articulos ;
+
+    public static ArrayList<Articulo> get() {
+        if (articulos == null ) {
+            articulos = loadArticulos();
+        }
+        return articulos ;
+    }
+
+    public static void nuevo(String codigo, String descripcion, float cantidad, float importe) {
+        Articulo articulo = new Articulo(codigo, descripcion, cantidad, importe) ;
+        articulos.add( articulo );
+    }
+
+    public static void modifica(int id, String codigo, String descripcion, float cantidad, float importe ) {
+        Articulo articulo = articulos.get( id );
+        articulo.setCodigo( codigo );
+        articulo.setDescripcion( descripcion );
+        articulo.setCantidad( cantidad );
+        articulo.setImporte( importe );
+
+    }
+
+    public static ArrayList<Articulo> loadArticulos() {
+        ArrayList<Articulo> resultado = new ArrayList<>() ;
+        /*
+        ArrayList<Articulo> articulos = new ArrayList<>() ;
+
+        articulos.add(new Articulo("cod1", "articulo 1", 2.0f, 12.20f ));
+        articulos.add(new Articulo("cod3", "articulo 3", 2.0f, 15.20f ));
+        resultado.add(new Pedido( 1 , new Cliente( 1 , "Leo Casales", "R.de Escalada", "223 4-213264"), articulos ) ) ;
+
+        ArrayList<Articulo> articulos2 = new ArrayList<>() ;
+        articulos2.add(new Articulo("cod2", "teclado USB", 2.0f, 12.20f ));
+        articulos2.add(new Articulo("cod4", "mouse USB", 1.0f, 9.20f ));
+        resultado.add(new Pedido( 2 , new Cliente( 2 , "Tizi Casales", "Mar del Plata", "470-1520" ), articulos2  ) ) ;
+        */
+
+        return resultado ;
+    }
+}
